fe7d73cd98b1d9bd5d15e986dd660f50a93d42df
solo <solomon.munyalo@student.moringaschool.com>
2021-05-06 18:56:22 +0300

    add first function

--- Diary_business.js
+++ Diary_business.js
@@ -1,5 +1,23 @@
-var productionshedA = 510
-var productionshedB = 510
-var productionshedC = 510
-var productionshedD = 510
-var productionshedE = 510
+var productionShedA = 510
+var productionShedB = 308
+var productionShedC = 486
+var productionShedD = 572
+
+let total_Production = productionShedA + productionShedB + productionShedC + productionShedD
+
+
+function totalProduction(){
+  console.log("Your production in Shed A "+ productionShedA + " litres per day")
+  console.log("Your production in Shed B "+ productionShedB + " litres per day")
+  console.log("Your production in Shed C "+ productionShedC + " litres per day")
+  console.log("Your production in Shed D "+ productionShedD + " litres per day")
+  console.log("The total production is " + total_Production + " litres per day")
+}
+
+totalProduction()
+
+incomeOverTime(selling_price, time);
+
+let function incomeOverTime(selling_price, time) {
+  
+}
